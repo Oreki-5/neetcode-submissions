c4fc2a6a195a801665cef1473b3b15fafd549b5e
Oreki-5 <138198574+Oreki-5@users.noreply.github.com>
2026-08-12 16:26:37 +0530

    Add: two-integer-sum-ii - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-2.java
@@ -0,0 +1,28 @@
+// import java.util.ArrayList;
+// import java.util.HashMap;
+// import java.util.List;
+// import java.util.Map;
+
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+
+        int L = 0;
+        int R = numbers.length-1;
+
+        while (L < R) {
+            if(numbers[L]+numbers[R] == target){
+                return new int[]{L+1,R+1};
+            }
+            else if(numbers[L]+numbers[R] > target){
+                R--;
+            }
+            else{
+                L++;
+            }
+
+        }
+
+        return null;    
+    }
+
+}
